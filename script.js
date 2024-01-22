0fd1a6fe00d26e1f85cc05c540d2f29d311c1baf
aRav3n <24660772+aRav3n@users.noreply.github.com>
2024-01-22 15:02:33 -0600

    Write function to check amount of key value pairs in hashMap

--- script.js
+++ script.js
@@ -7,6 +7,7 @@
 
 const hashMap = function () {
   const loadFactor = 0.8;
+  const map = [];
   // function src: https://www.theodinproject.com/lessons/javascript-hashmap-data-structure#collisions
   const hash = function (string) {
     let hashCode = 0;
@@ -18,4 +19,32 @@ const hashMap = function () {
 
     return hashCode;
   };
+
+  // length() returns the number of stored keys
+  const length = function () {
+    let countOfKeyValuePairs = 0;
+    const mapLength = map.length;
+    for (let i = 0; i < mapLength; i++) {
+      if (map[i] !== undefined) {
+        countOfKeyValuePairs += map[i].length;
+      }
+    }
+  };
+
+  // set(key, value) if key exists already then overwrite.
+  // update table size if >= loadFactor capacity
+
+  // get(key) returns value else null
+
+  // keys() returns array containing all keys
+
+  // has(key) returns a boolean
+
+  // remove(key) returns a boolean
+
+  // clear() clears the hashTable
+
+  // values() returns an array with all values in hashTable
+
+  // entries() returns an array containing all key value pairs
 };
